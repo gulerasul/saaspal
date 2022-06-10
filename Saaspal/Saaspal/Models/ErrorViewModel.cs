0d2b548399e60f59973e6f129cff8793ccf8d1a4
Guler Resulova <tu699evkp@aztu.local>
2022-06-10 15:44:51 +0400

    first commit

---
+++ Saaspal/Saaspal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Saaspal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
